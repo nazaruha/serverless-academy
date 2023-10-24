4441971a1eef7b69327d6042e69f7ec77519c4e3
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-10-24 21:04:16 +0200

    all operations are done!

---
+++ 01_cli_interactive_sort/arr-methods.js
@@ -0,0 +1,40 @@
+class Arr {
+    static GetArrStrings = (arr) => {
+        var newArr = new Array();
+    
+        arr.forEach(element => {
+            if (isNaN(element)) {
+                newArr.push(element);
+            }
+        });
+    
+        return newArr;
+    }
+    
+    static GetArrNumbers = (arr) => {
+        var newArr = new Array();
+    
+        arr.forEach(element => {
+            if (!isNaN(element)) {
+                newArr.push(Number(element));
+            }
+        });
+    
+        return newArr;
+    }
+
+    static SortAlphabetic = (arr) => arr.sort((a,b) => a.localeCompare(b));
+
+    static SortLetterCountAscending = (arr) => arr.sort((a,b) => a.length - b.length);
+
+    static GetUniqueValues = (arr) => {
+        const set = new Set(arr);
+        return [...set];
+    }
+
+    static SortNumbersAscending = (arr) => arr.sort((a,b) => a-b);
+
+    static SortNumbersDescending = (arr) => arr.sort((a,b) => b-a);
+}
+
+module.exports = Arr
--- 01_cli_interactive_sort/index.js
+++ 01_cli_interactive_sort/index.js
@@ -1,17 +1,23 @@
 const readLine = require("./readline");
+const Arr = require("./arr-methods");
 
 const menu = () => {
     readLine.question('Hello. Enter 10 words or digits dividing them in spaces: ', (userInput) => {
         var choice = "";
         const arr = userInput.split(' ');
-        console.log(arr);
+        const arrStr = Arr.GetArrStrings(arr);
+        const arrNum = Arr.GetArrNumbers(arr);
+        // console.log(arr);
+        // console.log(arrStr);
+        // console.log(arrNum);
     
         console.log("How would you like to sort values:");
         console.log("1. Words by name (form A to Z).");
         console.log("2. Show digits from the smallest.");
         console.log("3. Show digits from the bigest.");
-        console.log("4. Words by quantity of leters.");
+        console.log("4. Words by quantity of letters.");
         console.log("5. Only unique words.");
+        console.log("6. Only unique words and numbers.");
         readLine.question("Select (1 - 5) and press ENTER: ", (choiceInput) => {
             choice = choiceInput.toLowerCase().trim();
             if (choice === 'exit') {
@@ -20,23 +26,27 @@ const menu = () => {
             } else {
                 switch(choice) {
                     case '1': {
-                        console.log(`Choice #${choice}`);
+                        console.log(Arr.SortAlphabetic(arrStr));
                         break;
                     }
                     case '2': {
-                        console.log(`Choice #${choice}`);
+                        console.log(Arr.SortNumbersAscending(arrNum));
                         break;
                     }
                     case '3': {
-                        console.log(`Choice #${choice}`);
+                        console.log(Arr.SortNumbersDescending(arrNum));
                         break;
                     }
                     case '4': {
-                        console.log(`Choice #${choice}`);
+                        console.log(Arr.SortLetterCountAscending(arrStr));
                         break;
                     }
                     case '5': {
-                        console.log(`Choice #${choice}`);
+                        console.log(Arr.GetUniqueValues(arrStr));
+                        break;
+                    }
+                    case '6': {
+                        console.log(Arr.GetUniqueValues(arr));
                         break;
                     }
                     default: {
