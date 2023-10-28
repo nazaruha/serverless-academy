669da957dc0df8dfd36cb872140fba0c752d42ab
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-10-28 13:52:34 +0200

    make successfull axios forecast request

--- 04_telegram_bot_weather_forecast/index.js
+++ 04_telegram_bot_weather_forecast/index.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+const axios = require("axios");
+const TelegramBot = require('node-telegram-bot-api');
+const {weatherAPI} = require("./objects");
+
+const weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${weatherAPI.lat}&lon=${weatherAPI.lon}&cnt=${weatherAPI.cnt["3"]}&appid=${weatherAPI.appid}&units=${weatherAPI.units["Celsius"]}&lang=${weatherAPI.lang["English"]}`;
+const token = "";
+
+axios.get(weatherURL)
+    .then((res) => {
+        const data = res.data;
+        const list = data.list;
+        const city = data.city;
+        const weather = list[0].weather[0];
+        console.log(weather.description)
+    }).catch(err => console.log(err));
+
---
+++ 04_telegram_bot_weather_forecast/objects.js
@@ -0,0 +1,22 @@
+const weatherAPI = {
+    lat: "50.6196175",
+    lon: "26.2513165",
+    appid: "18a2165c7cf02561da97f430a93fd424",
+    cnt: {
+        '3': 3,
+        '6': 6
+    },
+    units: {
+        "Fahrenheit":  "imperial",
+        "Celsius": "metric",
+        "Kelvin": "" // default
+    },
+    lang: {
+        "Ukrainian": "uk",
+        "English": "en"
+    }
+}
+
+module.exports = {
+    weatherAPI
+};
