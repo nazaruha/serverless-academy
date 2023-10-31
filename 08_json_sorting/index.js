b89f48107f790d2731fcfb607c01e338a269985e
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-10-31 13:33:30 +0100

    count True and False values but endpoints doesnt still work

--- 08_json_sorting/index.js
+++ 08_json_sorting/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs').promises;
 const fullURLs = require("./objects");
 
+var countTrue = 0;
+var countFalse = 0;
 async function MakeRequest(url) {
     let count = 3;
     for (let i = 0; i < count; i++) {
@@ -8,8 +10,10 @@ async function MakeRequest(url) {
             const resp = await fetch(url);
             if (resp.ok) {
                 if (resp.includes("\"isDone\": true") || resp.includes("\'isDone\': true") || resp.includes("isDone: true")) {
+                    countTrue++;
                     return `[Success] ${url}: isDone - True`;
                 }
+                countFalse++;
                 return `[Success] ${url}: isDone - False`;
             }
         } catch(err) {
@@ -23,4 +27,5 @@ async function MakeRequest(url) {
     for (let i = 0; i < fullURLs.length; i++) {
         console.log(await MakeRequest(fullURLs[i]))
     }
+    console.log(`\nFount True values: ${countTrue},\nFound False values: ${countFalse}`);
 })();
