c8227647c94dd68a9f44dae44804251f54f095c7
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-10-31 13:30:37 +0100

    make isDone validation... maybe 'cause endpoints dont work

--- 08_json_sorting/index.js
+++ 08_json_sorting/index.js
@@ -1,1 +1,26 @@
-console.log("Hello, World!");
+const fs = require('fs').promises;
+const fullURLs = require("./objects");
+
+async function MakeRequest(url) {
+    let count = 3;
+    for (let i = 0; i < count; i++) {
+        try {
+            const resp = await fetch(url);
+            if (resp.ok) {
+                if (resp.includes("\"isDone\": true") || resp.includes("\'isDone\': true") || resp.includes("isDone: true")) {
+                    return `[Success] ${url}: isDone - True`;
+                }
+                return `[Success] ${url}: isDone - False`;
+            }
+        } catch(err) {
+            // Handling Error
+        }
+    }
+    return `[Fail] ${url}: The endpoint is unavailable`;
+}
+
+(async () => {
+    for (let i = 0; i < fullURLs.length; i++) {
+        console.log(await MakeRequest(fullURLs[i]))
+    }
+})();
---
+++ 08_json_sorting/objects.js
@@ -0,0 +1,27 @@
+const url = 'https://jsonbase.com/sls-team/json-';
+const endpoints = [
+    '793',
+    '955',
+    '231',
+    '931',
+    '93',
+    '342',
+    '770',
+    '491',
+    '281',
+    '718',
+    '310',
+    '806',
+    '469',
+    '258',
+    '516',
+    '79',
+    '706',
+    '521',
+    '350',
+    '64'
+];
+const fullURLs = endpoints.map(endpoint => url + endpoint);
+
+
+module.exports = fullURLs;
